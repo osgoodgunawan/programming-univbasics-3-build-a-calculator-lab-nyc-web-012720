ede373e1f92b4e01c0a4aa24b03e157118d8f064
Osgood Gunawan <osgoodgunawan@hotmail.com>
2020-01-04 22:47:35 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,18 +5,19 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  minus=num1-num2
+  minus=num2-num1
   puts minus
   return minus
-
 end
 
 def division(num1, num2)
-
+  divide=num2/num1
+  puts divide
+  divide
 end
 
 def multiplication(num1, num2)
-
+  multiplu
 end
 
 def modulo(num1, num2)
