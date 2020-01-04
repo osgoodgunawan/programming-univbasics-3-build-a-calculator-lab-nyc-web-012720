fc5275f43524dc9b448ee684bf6c7b094ee25171
Osgood Gunawan <osgoodgunawan@hotmail.com>
2020-01-04 22:54:10 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,35 @@
 def addition(num1, num2)
-
+  sum=num1+num2
+  puts sum
+  sum
 end
 
 def subtraction(num1, num2)
-
+  minus=num1-num2
+  puts minus
+  return minus
 end
 
 def division(num1, num2)
-
+  divide=num1/num2
+  puts divide
+  divide
 end
 
 def multiplication(num1, num2)
-
+  multiply=num1*num2
+  puts multiply
+  multiply
 end
 
 def modulo(num1, num2)
-
+  remainder=num1%num2
+  puts remainder
+  remainder
 end
 
 def square_root(num)
-
+  square=Math.sqrt(num)
+  puts square
+  square
 end
